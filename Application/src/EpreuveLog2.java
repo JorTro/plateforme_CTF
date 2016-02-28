7a052e1476e92bb675d855bbeef5e63c0948f6b2
jordan <jordan.troesch@telecom-sudparis.eu>
2016-02-28 10:31:23 +0100

    EpreuveLog2.Java 00

---
+++ Application/src/EpreuveLog2.java
@@ -0,0 +1,45 @@
+import javax.swing.JOptionPane;
+
+public class EpreuveLog2 {
+
+	public void creationEpreuve(String suite, int solution) {
+		
+		int n = 0;
+		Boolean b1 = false;
+		Boolean b2 = false;
+		
+		while (!b1) {
+		
+			String reponse = JOptionPane.showInputDialog(null, suite + "\n" + "Entrez votre réponse : ", "Epreuve de logique n°2"
+					+ "", JOptionPane.QUESTION_MESSAGE);
+			if(reponse == null) {
+				System.exit(0);
+			}
+			while(!b2){
+				try {
+					n = Integer.parseInt(reponse);
+					b2 = true;
+				}
+				catch (NumberFormatException e) {
+					if(reponse == null) {
+						System.exit(0);
+					}
+					JOptionPane.showMessageDialog(null, "Valeur entrée invalide. Veuillez réessayer.", "Epreuve de logique n°2"
+							+ "", JOptionPane.ERROR_MESSAGE);
+					reponse = JOptionPane.showInputDialog(null, "Voici votre épreuve :  " + suite, "Epreuve de logique n°2"
+							+ "", JOptionPane.QUESTION_MESSAGE);
+				}
+			}
+		
+			if(n == solution) {
+				JOptionPane.showMessageDialog(null, "Epreuve réussie !", "Epreuve de logique n°2", JOptionPane.INFORMATION_MESSAGE);
+				b1 = true;
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Mauvaise réponse. Essayez encore.", "Epreuve de logique n°2"
+						+ "", JOptionPane.ERROR_MESSAGE);
+				b2 = false;
+			}
+		}
+	}
+}
