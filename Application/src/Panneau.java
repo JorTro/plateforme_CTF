7eecddc288317b7bc44fe82649e2f8137550e9f7
jordan <jordan.troesch@telecom-sudparis.eu>
2016-02-28 10:42:47 +0100

    Panneau.Java 00

---
+++ Application/src/Panneau.java
@@ -0,0 +1,12 @@
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class Panneau extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void paintComponent(Graphics g) {
+	}
+}
